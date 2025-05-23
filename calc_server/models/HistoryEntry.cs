a2f3177b34381e704647e488a565adcfdce88e67
Ofri Kuperberg <kuperbergof@mta.ac.il>
2025-05-23 21:57:26 +0300

    nullables

--- calc_server/models/HistoryEntry.cs
+++ calc_server/models/HistoryEntry.cs
@@ -1,9 +1,10 @@
+// ReSharper disable InconsistentNaming
 namespace calc_server.models;
 
 public class HistoryEntry
 {
-    public string flavor { get; set; }  // "STACK" or "INDEPENDENT"
-    public string operation { get; set; }
-    public List<int> arguments { get; set; }
-    public int result { get; set; }
+    public string? flavor { get; init; }  // "STACK" or "INDEPENDENT"
+    public string? operation { get; init; }
+    public List<int>? arguments { get; init; }
+    public int result { get; init; }
 }
